9e771f63cdbd299e4d1f56adad95fee959c36d8a
Raj Kumar Karn <132379680+RAJKUMARKARN@users.noreply.github.com>
2025-11-19 21:05:28 +0530

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
+import ScheduleDonation from "./pages/ScheduleDonation"; 
+import RequestBlood from "./pages/RequestBlood";
+  // ✅ ADD THIS
+
 
 function App() {
   return (
@@ -9,6 +13,8 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/schedule-donation" element={<ScheduleDonation />} />
+      <Route path="/request-blood" element={<RequestBlood />} />
     </Routes>
   );
 }
