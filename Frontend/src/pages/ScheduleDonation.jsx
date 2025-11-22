234f484045f56d16a72d5c8593a1e9768bffb64a
RAJKUMAR KARN <rajkumarkarn002@gmail.com>
2025-11-22 21:24:00 +0530

    fix

--- Frontend/src/pages/ScheduleDonation.jsx
+++ Frontend/src/pages/ScheduleDonation.jsx
@@ -8,11 +8,18 @@ export default function ScheduleDonation() {
   return (
     <Sidebar>
       <div className="p-10">
+        <div className="transition-all duration-300 flex ml-4 mt-4">
+          <img src="/logo.png" alt="" className="w-[48px] mt-[2px] h-[48px]" />
+          <div className="leading-tight flex flex-col justify-center ml-[5px]">
+            <h1 className="text-[28px] font-bold">Life Flows</h1>
+            <p className="text-[12px] font-medium text-[#969696]">Together we flow</p>
+          </div>
+        </div>
         <h1 className="text-4xl font-bold">Schedule Donation</h1>
         <p className="text-gray-500 mt-4">
           This is the schedule donation page.
         </p>
-        <Footer/>
+        <Footer className="b-[3px]"/>
       </div>
     </Sidebar>
   );
